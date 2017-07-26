2ecf4e362d25838643e5707930526b35e6e921ab
jjjjjeffrey <jjjjjeffreyzeng@gmail.com>
2017-07-26 16:28:03 +0800

    modify register and login requet path

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -7,8 +7,8 @@ extension Droplet {
         
         post("smsCode", handler: SMSController().sendSMSCode)
         post("authSMSCode", handler: SMSController().authSMSCode)
-        post("userRegister", handler: PassportController().mobileRegister)
-        post("userLogin", handler: PassportController().mobileLogin)
+        post("register", handler: PassportController().mobileRegister)
+        post("login", handler: PassportController().mobileLogin)
         
         let auth = grouped(AuthenticationMiddleware())
         auth.get("user", handler: PassportController().getUserInfo)
