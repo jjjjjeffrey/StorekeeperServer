129c37f437e347745f906dd809ce2360bd720b05
jjjjjeffrey <jjjjjeffreyzeng@gmail.com>
2017-07-27 13:31:43 +0800

    add shopName to register api

--- Sources/App/Controllers/PassportController.swift
+++ Sources/App/Controllers/PassportController.swift
@@ -29,13 +29,14 @@ final class PassportController {
     
     func mobileRegister(_ req: Request) throws -> ResponseRepresentable {
         
-        guard let mobile = req.data["mobile"]?.string, let password = req.data["password"]?.string else {
+        guard let mobile = req.data["mobile"]?.string,
+            let shopName = req.data["shopName"]?.string,
+            let password = req.data["password"]?.string else {
             return AppResponse(code: AppResponseCode.parameterError)
         }
         
         guard let _ = try User.makeQuery().filter("mobile", .equals, mobile).first() else {
-            let index = mobile.index(mobile.startIndex, offsetBy: 7)
-            let user = User(mobile: mobile, password: password, shopName: mobile.substring(from: index))
+            let user = User(mobile: mobile, password: password, shopName: shopName)
             try user.save()
             
             return AppResponse(data: try user.makeJSON())
