4b5652568838758160c4d8fc8b2e9d1c63915dca
jjjjjeffrey <jjjjjeffreyzeng@gmail.com>
2017-08-01 09:35:48 +0800

    add create and delete goods category api

--- Sources/App/Controllers/GoodsCategoryController.swift
+++ Sources/App/Controllers/GoodsCategoryController.swift
@@ -10,6 +10,18 @@ import Foundation
 import Vapor
 import HTTP
 
+enum GoodsCategoryResponseCode: Int, CustomResponsCode {
+    case categoryExist = 1101
+    
+    var description: String {
+        get {
+            switch self {
+            case .categoryExist: return "该分类已存在"
+            }
+        }
+    }
+}
+
 final class GoodsCategoryController: ResourceRepresentable {
     
     /// GET /goodsCategory
@@ -26,50 +38,30 @@ final class GoodsCategoryController: ResourceRepresentable {
     
     /// POST /goodsCategory
     func create(_ req: Request) throws -> ResponseRepresentable {
+        
+        guard let id = try req.user()?.id else {
+            throw Abort.unauthorized
+        }
+
         let category = try req.goodscategory()
-        try category.save()
-        return category
-    }
-    
-    /// GET /goodsCategory/13rd88
-    func show(_ req: Request, category: GoodsCategory) throws -> ResponseRepresentable {
-        return category
+        
+        let cs = try GoodsCategory.makeQuery().filter("name", .equals, category.name)
+        if let _  = try cs.first() {
+            return AppResponse(code: GoodsCategoryResponseCode.categoryExist)
+        } else {
+            category.userId = id
+            try category.save()
+            return category
+        }
+        
     }
     
     /// DELETE /goodsCategory/13rd88
     func delete(_ req: Request, category: GoodsCategory) throws -> ResponseRepresentable {
         try category.delete()
-        return Response(status: .ok)
+        return AppResponse()
     }
     
-    
-    /// When the user calls 'PATCH' on a specific resource, we should
-    /// update that resource to the new values.
-    func update(_ req: Request, category: GoodsCategory) throws -> ResponseRepresentable {
-        // See `extension Post: Updateable`
-        try category.update(for: req)
-        
-        // Save an return the updated post.
-        try category.save()
-        return category
-    }
-    
-    /// When a user calls 'PUT' on a specific resource, we should replace any
-    /// values that do not exist in the request with null.
-    /// This is equivalent to creating a new GoodsCategory with the same ID.
-    func replace(_ req: Request, category: GoodsCategory) throws -> ResponseRepresentable {
-        // First attempt to create a new Post from the supplied JSON.
-        // If any required fields are missing, this request will be denied.
-        let new = try req.goodscategory()
-        
-        // Update the post with all of the properties from
-        // the new post
-        category.name = new.name
-        try category.save()
-        
-        // Return the updated post
-        return category
-    }
     
     /// When making a controller, it is pretty flexible in that it
     /// only expects closures, this is useful for advanced scenarios, but
@@ -79,9 +71,6 @@ final class GoodsCategoryController: ResourceRepresentable {
         return Resource(
             index: index,
             store: create,
-            show: show,
-            update: update,
-            replace: replace,
             destroy: delete
         )
     }
